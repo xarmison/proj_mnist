892b5a071c0faf13811d66e8d5116ae0f6e1a1ce
Richardson Santiago <richardsonsantiago@ufrn.edu.br>
2022-05-15 11:09:46 -0300

    Home page

---
+++ source/static/custom.js
@@ -0,0 +1,101 @@
+// Canvas setup
+let canvas = new fabric.Canvas('canvas');
+canvas.isDrawingMode = true;
+canvas.freeDrawingBrush.width = 20;
+canvas.freeDrawingBrush.color = "#ffffff";
+canvas.backgroundColor = "#000000";
+canvas.renderAll();
+
+
+// Clear button callback
+$("#clear-canvas").click(() => {
+	canvas.clear();
+	canvas.backgroundColor = "#000000";
+	canvas.renderAll();
+	updateChart(zeros);
+	$("#status").removeClass();
+});
+
+
+// Predict button callback
+$("#predict").click(() => {
+
+	// Change status indicator
+	$("#status").removeClass().toggleClass("fa fa-spinner fa-spin");
+
+	// Get canvas contents as url
+	let fac = (1.) / 13.;
+	let drawing_data = canvas.toDataURLWithMultiplier('png', fac);
+	
+	// Post url to python script
+	let jq = $.post(`/predict/?drawing_data=${drawing_data}`)
+		.done( (json) => {
+			if (json.result) {
+				$("#status").removeClass().toggleClass("fa fa-check");
+				$('#svg-chart').show();
+				updateChart(json.data);
+			} else {
+				$("#status").removeClass().toggleClass("fa fa-exclamation-triangle");
+				console.log(`Script Error: {json.error}`);
+			}
+		})
+		.fail( (xhr, textStatus, error) => {
+			$("#status").removeClass().toggleClass("fa fa-exclamation-triangle");
+			console.log("POST Error: " + xhr.responseText + ", " + textStatus + ", " + error);
+		}
+		);
+
+});
+
+// Iniitialize d3 bar chart
+$('#svg-chart').hide();
+let labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let zeros = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+let margin = { top: 0, right: 0, bottom: 20, left: 0 },
+	width = 360 - margin.left - margin.right,
+	height = 180 - margin.top - margin.bottom;
+
+let svg = d3.select("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform",
+		"translate(" + margin.left + "," + margin.top + ")");
+
+let x = d3.scale.ordinal()
+	.rangeRoundBands([0, width], .1)
+	.domain(labels);
+
+let y = d3.scale.linear()
+	.range([height, 0])
+	.domain([0, 1]);
+
+let xAxis = d3.svg.axis()
+	.scale(x)
+	.orient("bottom")
+	.tickSize(0);
+
+svg.selectAll(".bar")
+	.data(zeros)
+	.enter().append("rect")
+	.attr("class", "bar")
+	.attr("x", function (d, i) { return x(i); })
+	.attr("width", x.rangeBand())
+	.attr("y", function (d) { return y(d); })
+	.attr("height", function (d) { return height - y(d); });
+
+svg.append("g")
+	.attr("class", "x axis")
+	.attr("transform", "translate(0," + height + ")")
+	.call(xAxis);
+
+// Update chart data
+function updateChart(data) {
+	d3.selectAll("rect")
+		.data(data)
+		.transition()
+		.duration(500)
+		.attr("y", function (d) { return y(d); })
+		.attr("height", function (d) { return height - y(d); });
+}
